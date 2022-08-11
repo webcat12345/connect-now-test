be2bda9a737c2d3c423104624d91491a585f7607
webcat12345 <rango0986@gmail.com>
2022-08-11 07:37:54 -0700

    feat: complete filter box ui

--- src/app/pages/video-games/video-games.module.ts
+++ src/app/pages/video-games/video-games.module.ts
@@ -1,21 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { VideoGamesRoutingModule } from './video-games-routing.module';
+
 import { VideoGamesComponent } from './video-games.component';
 import { FilterComponent } from './filter/filter.component';
 import { VideoGameListItemComponent } from './video-game-list-item/video-game-list-item.component';
 
-
 @NgModule({
-  declarations: [
-    VideoGamesComponent,
-    FilterComponent,
-    VideoGameListItemComponent
-  ],
-  imports: [
-    CommonModule,
-    VideoGamesRoutingModule
-  ]
+  declarations: [VideoGamesComponent, FilterComponent, VideoGameListItemComponent],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, VideoGamesRoutingModule],
 })
-export class VideoGamesModule { }
+export class VideoGamesModule {}
