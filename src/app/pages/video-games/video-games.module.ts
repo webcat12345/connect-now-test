27ddd03496d33817805c2b3022f74454049a7f48
webcat12345 <rango0986@gmail.com>
2022-08-11 08:34:43 -0700

    feat: video game fetch api integration

--- src/app/pages/video-games/video-games.module.ts
+++ src/app/pages/video-games/video-games.module.ts
@@ -7,9 +7,10 @@ import { VideoGamesRoutingModule } from './video-games-routing.module';
 import { VideoGamesComponent } from './video-games.component';
 import { FilterComponent } from './filter/filter.component';
 import { VideoGameListItemComponent } from './video-game-list-item/video-game-list-item.component';
+import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
   declarations: [VideoGamesComponent, FilterComponent, VideoGameListItemComponent],
-  imports: [CommonModule, FormsModule, ReactiveFormsModule, VideoGamesRoutingModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, VideoGamesRoutingModule, SharedModule],
 })
 export class VideoGamesModule {}
